f609bccf3ea2a771a6789341f2577fca60caa8f9
Christian Kadiu - christian.kadiu@studio.unibo.it <christian.kadiu@studio.unibo.it>
2024-11-26 15:45:08 +0100

    solved exercise 101 and first part of 102

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberApp.java
@@ -1,72 +1,93 @@
 package it.unibo.mvc;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.List;
+import java.util.StringTokenizer;
 
 /**
- */
+*/
 public final class DrawNumberApp implements DrawNumberViewObserver {
-    private static final int MIN = 0;
-    private static final int MAX = 100;
-    private static final int ATTEMPTS = 10;
+   private static final int MIN = 0;
+   private static final int MAX = 100;
+   private static final int ATTEMPTS = 10;
 
-    private final DrawNumber model;
-    private final List<DrawNumberView> views;
+   private final DrawNumber model;
+   private final List<DrawNumberView> views;
 
-    /**
-     * @param views
-     *            the views to attach
-     */
-    public DrawNumberApp(final DrawNumberView... views) {
-        /*
-         * Side-effect proof
-         */
-        this.views = Arrays.asList(Arrays.copyOf(views, views.length));
-        for (final DrawNumberView view: views) {
-            view.setObserver(this);
-            view.start();
-        }
-        this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
-    }
+   /**
+    * @param views
+    *              the views to attach
+    */
+   public DrawNumberApp(final DrawNumberView... views) throws IOException {
+       /*
+        * Side-effect proof
+        */
+       this.views = Arrays.asList(Arrays.copyOf(views, views.length));
+       for (final DrawNumberView view : views) {
+           view.setObserver(this);
+           view.start();
+       }
+       Configuration.Builder builder = new Configuration.Builder();
+       this.model = new DrawNumberImpl(MIN, MAX, ATTEMPTS);
+       BufferedReader reader = new BufferedReader(
+               new InputStreamReader(ClassLoader.getSystemResourceAsStream("config.yml")));
+       String line;
+       while ((line = reader.readLine()) != null) {
+           StringTokenizer st = new StringTokenizer(line);
+           while (st.hasMoreTokens()) {
+               String s = st.nextToken();
+               if (s.matches("minimum:")) {
+                   builder.setMin(Integer.parseInt(st.nextToken()));
+               } else if (s.matches("maximum:")) {
+                   builder.setMax(Integer.parseInt(st.nextToken()));
+               } else if (s.matches("attempts:")) {
+                   builder.setAttempts(Integer.parseInt(st.nextToken()));
+               }
+           }
+       }
+   }
 
-    @Override
-    public void newAttempt(final int n) {
-        try {
-            final DrawResult result = model.attempt(n);
-            for (final DrawNumberView view: views) {
-                view.result(result);
-            }
-        } catch (IllegalArgumentException e) {
-            for (final DrawNumberView view: views) {
-                view.numberIncorrect();
-            }
-        }
-    }
+   @Override
+   public void newAttempt(final int n) {
+       try {
+           final DrawResult result = model.attempt(n);
+           for (final DrawNumberView view : views) {
+               view.result(result);
+           }
+       } catch (IllegalArgumentException e) {
+           for (final DrawNumberView view : views) {
+               view.numberIncorrect();
+           }
+       }
+   }
 
-    @Override
-    public void resetGame() {
-        this.model.reset();
-    }
+   @Override
+   public void resetGame() {
+       this.model.reset();
+   }
 
-    @Override
-    public void quit() {
-        /*
-         * A bit harsh. A good application should configure the graphics to exit by
-         * natural termination when closing is hit. To do things more cleanly, attention
-         * should be paid to alive threads, as the application would continue to persist
-         * until the last thread terminates.
-         */
-        System.exit(0);
-    }
+   @Override
+   public void quit() {
+       /*
+        * A bit harsh. A good application should configure the graphics to exit by
+        * natural termination when closing is hit. To do things more cleanly, attention
+        * should be paid to alive threads, as the application would continue to persist
+        * until the last thread terminates.
+        */
+       System.exit(0);
+   }
 
-    /**
-     * @param args
-     *            ignored
-     * @throws FileNotFoundException 
-     */
-    public static void main(final String... args) throws FileNotFoundException {
-        new DrawNumberApp(new DrawNumberViewImpl());
-    }
+   /**
+    * @param args
+    *             ignored
+    * @throws FileNotFoundException
+    */
+   public static void main(final String... args) throws FileNotFoundException, IOException {
+       new DrawNumberApp(new DrawNumberViewImpl(), new DrawNumberViewImpl(), );
+   }
 
 }
